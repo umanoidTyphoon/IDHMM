ad3818b6f791385685673d2f4367fff51967a834
umanoidTyphoon <umanoidTyphoon@tiscali.com>
2015-06-12 16:32:43 +0200

    Insertion of transition and observation models to prove IDHMM correctness

--- src/IDHMM.py
+++ src/IDHMM.py
@@ -3,17 +3,35 @@ __author__ = 'umanoidTyphoon'
 import numpy as np
 from numpy import matrix
 
+# State related to elliptic point doubling
+D = 0
+# State related to elliptic point addition
+AD = 1
+# State related to random elliptic point addition
+RAD = 2
+IDHMM_STATES = {D, AD, RAD}
+
+
 class IDHMM:
     def __init__(self, key="0", trace_list=[]):
         self.key = key
         self.trace_list = trace_list
 
         self.belief = init_belief(self.key)
+
+        # ---------------------------------------------- CORRECTNESS TEST ----------------------------------------------
+        self.observation_model = init_observation_model_test()
+        self.transition_models = init_transition_models_test()
+        # ########################################### END CORRECTNESS TEST #############################################
+
         self.observation_model = init_observation_model()
         self.transition_models = init_transition_models()
 
+    def get_observation_model(self):
+        return self.observation_model
+
     def infer(self):
-        multitrace_inference(self.belief, self.key, self.trace_list)
+        multitrace_inference(self.belief, self.key, self.transition_models, self.observation_model, self.trace_list)
 
 
 def init_belief(key):
@@ -25,15 +43,31 @@ def init_belief(key):
 
 
 def init_observation_model():
-    # |-----|--------|---------|
-    # |     |   OD   |   OAD   |
-    # |-----|--------|---------|
-    # |  D  |   .5   |  .0  | .5  |
-    # |-----|-----|------|-----|
-    # | AD  | .5  |  .0  | .5  |
-    # |-----|-----|------|-----|
-    # | RAD | .5  |  .0  | .5  |
-    # |-----|-----|------|-----|
+    # |-----|---------|---------|
+    # |     |   OD    |   OAD   |
+    # |-----|---------|---------|
+    # |  D  |   1.0   |   0.0   |
+    # |-----|---------|---------|
+    # | AD  |   0.0   |   0.5   |
+    # |-----|---------|---------|
+    # | RAD |   0.0   |   0.5   |
+    # |-----|---------|---------|
+    model = matrix([[1.0, .0], [.0, .5], [.0, .5]])
+
+    return model
+
+
+def init_observation_model_test():
+    # |-----|---------|---------|
+    # |     |   OD    |   OAD   |
+    # |-----|---------|---------|
+    # |  D  |   1.0   |   0.0   |
+    # |-----|---------|---------|
+    # | AD  |   0.0   |   1.0   |
+    # |-----|---------|---------|
+    model = matrix([[1.0, .0], [.0, 1.0]])
+
+    return model
 
 
 def init_transition_models():
@@ -69,12 +103,65 @@ def init_transition_models():
     return models
 
 
-def singletrace_inference(belief, trace):
+def init_transition_models_test():
+    models = []
+    # Key bit is 0
+    # |-----|---------|---------|
+    # |     |    D    |    AD   |
+    # |-----|---------|---------|
+    # |  D  |   1.0   |   .0    |
+    # |-----|---------|---------|
+    # | AD  |   1.0   |   .0    |
+    # |-----|---------|---------|
+    transition_model_key_bit0 = matrix([[1.0, .0], [1.0, .0]])
+
+    # Key bit is 1
+    # |-----|---------|---------|
+    # |     |    D    |    AD   |
+    # |-----|---------|---------|
+    # |  D  |   .0    |   1.0   |
+    # |-----|---------|---------|
+    # | AD  |   .0    |   1.0   |
+    # |-----|---------|---------|
+    transition_model_key_bit1 = matrix([[.0, 1.0], [.0, 1.0]])
+
+    # print "Transition model associated to key bit 0:\n", transition_model_key_bit0
+    # print "Transition model associated to key bit 1:\n", transition_model_key_bit1
+
+    models.append(transition_model_key_bit0); models.append(transition_model_key_bit1)
+    return models
+
+
+def compute_alpha_parm(belief, transition_models, observation_model):
+
+    print observation_model
+    print "x"
+    print transition_models[1][AD].item(AD)
+    print "Belief", belief.item(0)
+
+    p_yi_given_q_i = observation_model[AD].item(AD)
+    p_qi_given_qprev_key_bit = transition_models[1][AD].item(AD)
+    p_ki = belief.item(0)
+
+    return p_yi_given_q_i * p_qi_given_qprev_key_bit * p_ki
+
+def singletrace_inference(belief, transition_models, observation_model, trace):
+    alpha_parm = None
+    beta_parm  = None
+    bayes_rule_numerator   = None
+    bayes_rule_denominator = None
+    updated_belief = None
+
+    alpha_parm = compute_alpha_parm(belief, transition_models, observation_model)
+
+    print "Alpha parameter computed: ", alpha_parm
     return
 
 
-def multitrace_inference(belief, key, trace_list):
+def multitrace_inference(belief, key, transition_models, observation_model, trace_list):
         print "Initial belief D_0:", belief
+        print observation_model
 
         for trace in trace_list:
-            belief = singletrace_inference(belief, trace)
+            print trace
+            belief = singletrace_inference(belief, transition_models, observation_model, trace)
--- test/IDHMMTester.py
+++ test/IDHMMTester.py
@@ -2,6 +2,6 @@ __author__ = 'umanoidTyphoon'
 
 from src.IDHMM import IDHMM
 
-idhmm = IDHMM("0101", [])
+idhmm = IDHMM("0101", ["AD"])
 
 idhmm.infer()
